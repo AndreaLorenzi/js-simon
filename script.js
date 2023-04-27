6ca1692e989b3396e0a00dd805d245174aae78aa
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-04-27 17:07:02 +0200

    add function

--- script.js
+++ script.js
@@ -2,3 +2,28 @@
 let countDownDate = new Date("april 28, 2023 09:30:00").getTime();
 
 
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+   
+    let now = new Date().getTime();
+  
+   
+    let distance = countDownDate - now;
+  
+    
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+  
+     
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+  }, 1000);
